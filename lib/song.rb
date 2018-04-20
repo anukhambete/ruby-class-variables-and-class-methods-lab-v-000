f876a89a08355d70cdba4c8b5c8c06129cff5813
Anuradha Khambete <anukhambete@gmail.com>
2018-04-20 14:17:05 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song
-  attr_accessor :song, :artist, :genre
+  attr_accessor :name, :artist, :genre
   
-  def initialize(song,artist,genre)
+  def initialize(name,artist,genre)
   end
   
   
